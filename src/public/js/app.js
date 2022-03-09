5fecb7c996d88e0e833f3394ebc6ce74a5271f4d
zzerowin <alaykr123@gmail.com>
2022-03-09 19:21:22 +0900

    live chat

---
+++ src/public/js/app.js
@@ -0,0 +1,45 @@
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+  const msg = {type, payload}
+  return JSON.stringify(msg);
+}
+
+function handleOpen() {
+  console.log("서버에 연결되었습니다 ✅");
+}
+
+socket.addEventListener("open", handleOpen);
+
+socket.addEventListener("message", (message) => {
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
+});
+
+socket.addEventListener("close", () => {
+  console.log("서버와 연결이 끊어졌습니다 ❌")
+})
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(makeMessage("new_message", input.value));
+  const li = document.createElement("li");
+  li.innerText = `당신: ${input.value}`;
+  messageList.append(li);
+  input.value = "";
+}
+
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+  input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
