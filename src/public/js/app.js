4c21f5821e8148e07185caab9fa773b768cdc4d5
zzerowin <alaykr123@gmail.com>
2022-03-09 21:40:14 +0900

    socket.io is amazing2

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,12 +3,14 @@ const socket = io();
 const welcome = document.getElementById("welcome");
 const form = welcome.querySelector("form");
 
+function backendDone(msg) {
+  console.log(`The backend says:`, msg);
+}
+
 function handleRoomSubmit(event) {
   event.preventDefault();
   const input = form.querySelector("input");
-  socket.emit("enter_room", { payload: input.value }, () => {
-    console.log("서버가 완료되었습니다!");
-  })
+  socket.emit("enter_room", input.value, backendDone);
   input.value = "";
 }
 
