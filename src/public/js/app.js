fe16d88cf8b3f69ae60c68f8ce8f0bd29d7b8fab
zzerowin <alaykr123@gmail.com>
2022-03-10 01:08:09 +0900

    room notifications2

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -15,11 +15,24 @@ function addMessage(message) {
   ul.appendChild(li);
 }
 
+function handleMessageSubmit(event) {
+  event.preventDefault();
+  const input = room.querySelector("input");
+  const value = input.value;
+  socket.emit("new_message", input.value, roomName, () => {
+    addMessage(`당신: ${value}`);
+  });
+  input.value = "";
+}
+
+
 function showRoom() {
   welcome.hidden = true;
   room.hidden = false;
   const h3 = room.querySelector("h3");
   h3.innerText = `${roomName} Room`;
+  const form = room.querySelector("form");
+  form.addEventListener("submit", handleMessageSubmit);
 }
 
 function handleRoomSubmit(event) {
@@ -35,3 +48,9 @@ form.addEventListener("submit", handleRoomSubmit);
 socket.on("welcome", () => {
   addMessage("누군가 입장하셨습니다!");
 });
+
+socket.on("bye", () => {
+  addMessage("누군가 나가셨습니다 ㅠㅠ");
+});
+
+socket.on("new_message", addMessage);
